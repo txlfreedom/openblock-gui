ecac4880a43a374969d0d4ae3447bd945364b9ff
Arthur <869705086@qq.com>
2021-03-05 02:47:54 +0000

    Build for 97a89dcadb9b24738caf75b5005111bbbfea6acd [skip ci]

--- desktop/chunks/player.js
+++ desktop/chunks/player.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[5],{
 
-/***/ 1893:
+/***/ 1896:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -95,7 +95,7 @@ react_dom__WEBPACK_IMPORTED_MODULE_3___default.a.render( /*#__PURE__*/react__WEB
 
 /***/ }),
 
-/***/ 1894:
+/***/ 1897:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(21)(false);
@@ -118,7 +118,7 @@ exports.locals = {
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(1894);
+var content = __webpack_require__(1897);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -140,5 +140,5 @@ if(false) {}
 
 /***/ })
 
-},[[1893,0]]]);
+},[[1896,0]]]);
 //# sourceMappingURL=player.js.map
